7f2598b90d3be8d9e0d14fea89e7b49b86f8a25c
Prachi Goyal <prachisgoyal14@gmail.com>
2021-11-28 16:02:51 -0500

    tests added.

--- src/components/_tests_/App.test.js
+++ src/components/_tests_/App.test.js
@@ -1,33 +1,13 @@
 import React from "react";
-import { fireEvent, render } from "@testing-library/react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "../Home";
+import { render, waitFor } from "@testing-library/react-native";
 
-describe("Testing react navigation", () => {
-  test("page contains the header", async () => {
-    const component = <Home />;
+import App from "../../../App";
 
-    const { findByText } = render(component);
+// ...
 
-    const header = findByText("Welcome");
-
-    expect(header).toBeTruthy();
-  });
-
-  test("clicking on Profile takes you to the Profile screen", async () => {
-    const navigate = jest.fn();
-    const component = <Home navigation={{ navigate }} />;
-
-    const { getByText } = render(component);
-    const toClick = getByText("Profile");
-
-    fireEvent.press(toClick);
-    //expect(mockedDispatch).toHaveBeenCalledTimes(1);
-    // const newHeader = findByText("Profile");
-    // const newBody = findByText("Gender");
-
-    // expect(newHeader).toBeTruthy();
-    // expect(newBody).toBeTruthy();
-    expect(navigate).toHaveBeenCalledWith("Profile");
+describe("App", () => {
+  it("renders home screen first on app launch", async () => {
+    const { getByText } = render(<App />);
+    await waitFor(() => getByText("Welcome"));
   });
 });
---
+++ src/components/_tests_/Interaction.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "../Home";
+
+describe("Testing react navigation", () => {
+  test("page contains the header", async () => {
+    const component = <Home />;
+
+    const { findByText } = render(component);
+
+    const header = findByText("Welcome");
+
+    expect(header).toBeTruthy();
+  });
+
+  test("clicking on Profile takes you to the Profile screen", async () => {
+    const navigate = jest.fn();
+    const component = <Home navigation={{ navigate }} />;
+    const { getByText } = render(component);
+    const toClick = getByText("Profile");
+
+    fireEvent.press(toClick);
+    expect(navigate).toHaveBeenCalledWith("Profile");
+  });
+});
